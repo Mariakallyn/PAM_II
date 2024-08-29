48f7e6aae06e478ea57d56d75a8b2ae4aa4bd538
Mariakallyn <kallynetenorio73@gmail.com>
2024-08-28 22:49:16 -0300

    branch com o código da aula do dia Wed, Aug 28, 2024 10:49:16 PM

---
+++ lib/componentes/filme_item.dart
@@ -0,0 +1,82 @@
+import 'package:cinemeu/entidade/filme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class FilmeItem extends StatelessWidget {
+  final Filme filme;
+
+  const FilmeItem({super.key, required this.filme});
+
+  @override
+  Widget build(BuildContext context) {
+    final tema = Theme.of(context);
+
+    return Container(
+      margin: const EdgeInsets.only(
+        left: 4,
+        right: 4,
+        top: 4,
+      ),
+      child: Material(
+        elevation: 5,
+        child: Padding(
+          padding: const EdgeInsets.all(4),
+          child: Row(
+            children: [
+              Image.network(
+                filme.poster,
+                height: 200,
+                fit: BoxFit.fitHeight,
+              ),
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  constraints: const BoxConstraints(
+                    minHeight: 200,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Text(
+                        filme.titulo,
+                        style: tema.textTheme.titleLarge,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            filme.duracao.toString(),
+                            style: tema.textTheme.bodySmall,
+                          ),
+                          Text(
+                            "Duração: ${filme.duracao}min",
+                            style: tema.textTheme.bodySmall,
+                          ),
+                          Text(
+                            "Classificação: ${filme.classificacao}",
+                            style: tema.textTheme.bodySmall,
+                          ),
+                        ],
+                      ),
+                      Container(
+                        width: double.maxFinite,
+                        margin: const EdgeInsets.only(top: 4),
+                        child: Text(
+                          filme.sinopse,
+                          style: tema.textTheme.bodySmall,
+                          textAlign: TextAlign.justify,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
